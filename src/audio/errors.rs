029d95d810fa06acfb40d7a92efb58145a62d161
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-10 14:23:26 +0200

    Update errors.rs

--- src/audio/errors.rs
+++ src/audio/errors.rs
@@ -1,31 +1,81 @@
-use rodio::StreamError;
+use cpal::DevicesError;
+use rodio::cpal::{BackendSpecificError, DeviceNameError};
+use rodio::{PlayError, StreamError, decoder::DecoderError};
+use std::error::Error;
 use std::io::ErrorKind;
 
-pub(super) enum AudioError {
-    AudioError,
-    FileError,
+#[derive(Debug)]
+pub enum AudioError {
+    //also temp
+    Default,
+    IoError,
     DecoderError,
     FileNotLoaded,
+    DeviceError(BackendSpecificError),
+    NoDevice,
+    //maybe unused
+    #[allow(unused)]
+    Other(Box<dyn Error + Send>),
 }
 
+//I still specify the full paths to show they're no std/core types
 impl From<rodio::StreamError> for AudioError {
     fn from(value: StreamError) -> Self {
         use rodio::StreamError as SE;
         match value {
             //temporary match-all
-            SE::NoDevice => AudioError::AudioError,
+            SE::NoDevice => AudioError::Default,
             //SE::
-            _ => AudioError::AudioError,
+            _ => AudioError::Default,
+        }
+    }
+}
+
+impl From<rodio::DevicesError> for AudioError {
+    fn from(value: rodio::DevicesError) -> Self {
+        use rodio::DevicesError as SE;
+        //will replace it later, is already a catch-all
+        match value {
+            SE::BackendSpecific { err } => AudioError::DeviceError(err),
+        }
+    }
+}
+
+impl From<rodio::cpal::DeviceNameError> for AudioError {
+    fn from(value: DeviceNameError) -> Self {
+        use DeviceNameError as DNE;
+        match value {
+            DNE::BackendSpecific { err } => AudioError::DeviceError(err),
         }
     }
 }
 
 impl From<std::io::Error> for AudioError {
     fn from(value: std::io::Error) -> Self {
-        use std::io::Error as IOError;
         match value {
             //temp
-            _ => AudioError::FileError,
+            _ => AudioError::Default,
+        }
+    }
+}
+
+impl From<rodio::PlayError> for AudioError {
+    fn from(value: PlayError) -> Self {
+        match value {
+            PlayError::NoDevice => AudioError::NoDevice,
+            //to be specified later
+            PlayError::DecoderError(_) => AudioError::DecoderError,
+        }
+    }
+}
+
+impl From<rodio::decoder::DecoderError> for AudioError {
+    fn from(value: rodio::decoder::DecoderError) -> Self {
+        match value {
+            //also kinda temp match-all
+            DecoderError::DecodeError(_) => AudioError::DecoderError,
+            DecoderError::IoError(_) => AudioError::IoError,
+            _ => AudioError::DecoderError,
         }
     }
 }
