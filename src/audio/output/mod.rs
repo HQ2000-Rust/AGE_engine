7d065cfb01308d0dac7ce0a61aecb2676b4bfda4
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-10 14:24:18 +0200

    Update mod.rs

--- src/audio/output/mod.rs
+++ src/audio/output/mod.rs
@@ -1,5 +1,12 @@
-pub(super) struct OutputDevice {
-    device: todo!(),
-}
+use super::device::Device;
+use crate::audio::errors::AudioError;
+use rodio::DeviceTrait;
 
+pub struct Output;
+impl Device<Output> {
+    fn get_name(&self) -> Result<String, AudioError> {
+        //needed because otherwise I'd also need to implement From<Result<String, DeviceNameError>> for Result<String, AudioError>...
+        Ok(self.device.name()?)
+    }
+}
 //re-exports
