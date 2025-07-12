f349379b36cbdc5002fa457ba637132a100838cd
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-12 22:10:54 +0200

    Update audio.rs

--- tests/audio.rs
+++ tests/audio.rs
@@ -1,11 +1,13 @@
 #![cfg(test)]
 
+use age_engine::audio::output_handle::OutputHandle;
 use std::path::Path;
-use age_engine::audio::audio_handles::OutputHandle;
 #[test]
 fn play_test() {
-    let handle=OutputHandle::default();
-    let mut handle=handle.activate_output_with_default_device().unwrap();
-    handle.load_file(Path::new("music.mp3"), String::from("1")).unwrap();
-    handle.play_loaded(String::from("1"),1).unwrap();
+    let handle = OutputHandle::default();
+    let mut handle = handle.activate_output().unwrap();
+    //handle.load_file("music.mp3", String::from("1")).unwrap();
+    //handle.play_loaded(String::from("1"),1).unwrap();
+    handle.play_from_file("music.mp3").unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
