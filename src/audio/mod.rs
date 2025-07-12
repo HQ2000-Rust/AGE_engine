d136be02f7839c655067394f653e67ba824f6419
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-12 22:08:24 +0200

    Update mod.rs

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,8 +1,7 @@
 //maybe not everything needs to be pub in the end, idk
-    pub mod audio_handles;
-    pub mod device;
-    pub mod errors;
-    pub mod input;
-    pub mod manipulation;
-    pub mod output;
-    pub mod traits;
+pub mod errors;
+pub mod input;
+pub mod manipulation;
+pub mod output;
+pub mod output_handle;
+pub mod traits;
